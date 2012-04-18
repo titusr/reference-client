654613d37ba34b2ee01505b831a2e3dc73017cef
Martijn Vogten <martijn@vogten.nl>
2012-04-18 10:56:33 +0200

    initial version

---
+++ oai_harvester.php
@@ -0,0 +1,57 @@
+<?php 
+
+function parseResumptionToken($xml) {
+  $resumption_xml = new SimpleXMLElement($xml);
+  $resumptionToken = array(
+      "expirationDate" => (string)$resumption_xml['expirationDate'],
+      "cursor" => (string)$resumption_xml['cursor'],
+      "resumptionToken" => (string)$resumption_xml
+  );
+  return $resumptionToken;
+}
+
+function harvestBatch($provider_url, $oai_params, $record_callback, $resumptionToken = null) {
+  $reader = new XMLReader();
+  
+  if ($resumptionToken) {
+    $url = $provider_url . "?verb=ListRecords&resumptionToken=" . rawurlencode($resumptionToken["resumptionToken"]);
+  } else {
+    $paramstr = "&";
+    foreach($oai_params as $key => $val) {
+      $paramstr .= $key . "=" . rawurlencode($val) . "&";
+    }
+    $url = $provider_url . "?verb=ListRecords" . $paramstr;
+  }
+  
+  if (!$reader->open($url)) throw new Exception("Could not open URL: " . $url);
+  
+  while($reader->read() && $reader->name !== 'record');
+  
+  while($reader->name === 'record') {
+    $xml = $reader->readOuterXml();
+    if (!$xml) throw new Exception("readOuterXml failed: " . $url);
+
+    $record_callback($xml);
+    
+    // Skip to the next element, ignore whitespace
+    do {
+       if (!$reader->next()) throw new Exception("reader->next() failed: " . $url);
+    } while ($reader->nodeType !== XMLReader::ELEMENT);
+  }
+  
+  $resumptionToken = null;
+  if ($reader->name === "resumptionToken") {
+    $resumptionToken = parseResumptionToken($reader->readOuterXml());
+  }
+  
+  $reader->close();
+  
+  return $resumptionToken;
+}
+
+function harvest_all($provider_url, $oai_params, $record_callback) {
+  $resumptionToken = null;
+  do {
+    $resumptionToken = harvestBatch($provider_url, $oai_params, $record_callback, $resumptionToken);
+  } while ($resumptionToken);
+}
