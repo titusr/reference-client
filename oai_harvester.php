61287660c2805efa3a1c38d15a629e4cb029da35
Martijn Vogten <martijn@vogten.nl>
2012-04-18 11:51:25 +0200

    commentaar toegevoegd

--- oai_harvester.php
+++ oai_harvester.php
@@ -1,16 +1,21 @@
 <?php 
 
-function parseResumptionToken($xml) {
-  $resumption_xml = new SimpleXMLElement($xml);
-  $resumptionToken = array(
-      "expirationDate" => (string)$resumption_xml['expirationDate'],
-      "cursor" => (string)$resumption_xml['cursor'],
-      "resumptionToken" => (string)$resumption_xml
-  );
-  return $resumptionToken;
-}
-
-function harvestBatch($provider_url, $oai_params, $record_callback, $resumptionToken = null) {
+/**
+ * Harvest a single batch of records from the OAI endpoint at $provider_url and call 
+ * the provided callback function with the full XML of each record.
+ * 
+ * @param string $provider_url base URL of the OAI endpoint
+ * @param array $oai_params additional parameters for the ListRecords 
+ *                 verb, like <code>from</code>, <code>metadataPrefix</code>, etc.
+ * @param string $record_callback name of the callback function
+ * @param array $resumptionToken optional parameter, if not null: resume harvesting
+ *             using this resumptionToken
+ * 
+ * @return resumptionToken info containing keys <code>expirationDate</code>, </code>cursor</code> and <code>resumptionToken</code>, or <code>null</code> if no resumptionToken present
+ * 
+ * @author Martijn Vogten
+ */
+function harvest($provider_url, $oai_params, $record_callback, $resumptionToken = null) {
   $reader = new XMLReader();
   
   if ($resumptionToken) {
@@ -49,9 +54,35 @@ function harvestBatch($provider_url, $oai_params, $record_callback, $resumptionT
   return $resumptionToken;
 }
 
+/**
+ * Harvest all records from the OAI endpoint at $provider_url and call 
+ * the provided callback function with the full XML of each record.
+ * 
+ * 
+ * @param string $provider_url base URL of the OAI endpoint
+ * @param array $oai_params additional parameters for the ListRecords 
+ *                 verb, like <code>from</code>, <code>metadataPrefix</code>, etc.
+ * @param string $record_callback name of the callback function
+ * 
+ * @return nothing
+ * 
+ * @author Martijn Vogten
+ */
 function harvest_all($provider_url, $oai_params, $record_callback) {
   $resumptionToken = null;
   do {
-    $resumptionToken = harvestBatch($provider_url, $oai_params, $record_callback, $resumptionToken);
+    $resumptionToken = harvest($provider_url, $oai_params, $record_callback, $resumptionToken);
   } while ($resumptionToken);
 }
+
+// Creates a PHP array containing resumptionToken info
+function parseResumptionToken($xml) {
+  $resumption_xml = new SimpleXMLElement($xml);
+  $resumptionToken = array(
+      "expirationDate" => (string)$resumption_xml['expirationDate'],
+      "cursor" => (string)$resumption_xml['cursor'],
+      "resumptionToken" => (string)$resumption_xml
+  );
+  return $resumptionToken;
+}
+
