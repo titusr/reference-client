c0b1ae006cdc4521f52d67d60968f158a27ad208
Martijn Vogten <martijn@vogten.nl>
2012-10-11 15:51:48 +0200

    wijzigingen Nico

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 require "oai_harvester.php";
 
+$counter = 0;
+
 function processRecord($xml) {
+  global $counter;
+//   if ($counter++ > 100) {
+//     die;
+//   }
   $record = new SimpleXMLElement($xml);
   $identifier = $record->header->identifier;
   $datestamp = $record->header->datestamp;
+  $setspec = $record->header->setSpec;
+  
   
 //   echo "<h1>" . htmlspecialchars($identifier) . "</h1>";
 //   echo "<p>" . htmlspecialchars($datestamp) . "</p>";
@@ -15,14 +23,16 @@ function processRecord($xml) {
         "title" => null,
         "description" => null
       );
-  foreach($record->metadata->children('http://www.openarchives.org/OAI/2.0/oai_dc/') as $dcrec) {
-    foreach($dcrec->children('http://purl.org/dc/elements/1.1/') as $prop) {
+  $recordXml = $record->metadata->record;
+
+  foreach($recordXml->children('http://purl.org/dc/elements/1.1/') as $prop) {
      $props[$prop->getName()] = (string)$prop;
-    }
   }
-  
+
   mysql_query(
       "UPDATE oai_harvest SET" .
+      " `type`='" . mysql_real_escape_string($props['type']) . "'," .
+      " setspec='" . mysql_real_escape_string($setspec) . "'," .
       " title='" . mysql_real_escape_string($props['title']) . "'," . 
       " description='" . mysql_real_escape_string($props['description']) . "'," . 
       " recordxml='" . mysql_real_escape_string($xml) . "'," . 
@@ -32,8 +42,10 @@ function processRecord($xml) {
   echo mysql_error();
   if (mysql_affected_rows() === 0) {
       mysql_query(
-          "INSERT INTO oai_harvest (identifier, datestamp, recordxml, title, description) VALUES (" .
-          "'" . mysql_real_escape_string($identifier) . "'," . 
+          "INSERT INTO oai_harvest (identifier, `type`, setspec, datestamp, recordxml, title, description) VALUES (" .
+          "'" . mysql_real_escape_string($identifier) . "'," .
+          "'" . mysql_real_escape_string($props['type']) . "', " .
+          "'" . mysql_real_escape_string($setspec) . "', " .
           "'" . mysql_real_escape_string($datestamp) . "'," . 
           "'" . mysql_real_escape_string($xml) . "'," . 
           "'" . mysql_real_escape_string($props['title']) . "'," . 
@@ -44,22 +56,27 @@ function processRecord($xml) {
   echo ".";
 }
 
-mysql_connect("localhost", "root", "");
-mysql_select_db("oaitest");
+mysql_connect("localhost", "root", "root");
+mysql_select_db("communityhub");
 
 function initDB() {
   mysql_query(<<<SQL
       CREATE TABLE IF NOT EXISTS `oai_harvest` (
-        `identifier` CHAR(255) NOT NULL, 
+        `identifier` CHAR(255) NOT NULL,
+        `type` CHAR(255) NOT NULL,
+        `setspec` CHAR(255) NOT NULL,
         `datestamp` DATETIME,
         `recordxml` LONGTEXT,
         `title` LONGTEXT,
         `description` LONGTEXT,
-        PRIMARY KEY (`identifier`)) ENGINE=InnoDb DEFAULT CHARSET=utf8
+        PRIMARY KEY (`identifier`),
+        KEY `type` (`type`))
+        ENGINE=InnoDb DEFAULT CHARSET=utf8
 SQL
   );
 }
 
 initDB();
 
-harvest_all("http://localhost:8010/fedora/oai", array("metadataPrefix" => "oai_dc"), "processRecord");
+// harvest_all("http://oai.555systems.nl/oai/", array("metadataPrefix" => "ese", "set" => "WFM"), "processRecord");
+harvest_all("http://oai.555systems.nl/oai/", array("metadataPrefix" => "ese"), "processRecord");
